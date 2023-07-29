37d05227e4968cc905766d90183d89028b5d1384
anasassad <lwalida1921@gmail.com>
2023-07-29 23:50:41 +0100

    Developping the logic for the dialog and its flow

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,17 @@ import {
 } from '@angular/platform-browser';
 import { ClarityModule } from '@clr/angular';
 import { IssueListComponent } from './issue-list/issue-list.component';
+import { IssueReportComponent } from './issue-report/issue-report.component';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { IssueReportComponent } from './issue-report/issue-report.component';
+import { ConfirmDialogComponent } from './confirm-dialog/confirm-dialog.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     IssueListComponent,
-    IssueReportComponent
+    IssueReportComponent,
+    ConfirmDialogComponent
   ],
   imports: [
     BrowserModule,
